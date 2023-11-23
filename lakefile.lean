fadff354a1aaaa54b29cbcaf9c21d34b6c901e0c
Seasawher <tellagoodstory@icloud.com>
2023-11-24 00:18:53 +0900

    mathlib のバージョンを固定する

--- lakefile.lean
+++ lakefile.lean
@@ -5,7 +5,7 @@ package «NTL» where
   -- add any package configuration options here
 
 require mathlib from git
-  "https://github.com/leanprover-community/mathlib4.git"
+  "https://github.com/leanprover-community/mathlib4.git" @ "e9bcfc6e583d0784ece32475ca6f450126ef286f"
 
 @[default_target]
 lean_lib «NTL» where
