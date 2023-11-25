e44bbc2b8dba0ff6a0ff808d21878cff3f8f4662
Seasawher <Seasawher@users.noreply.github.com>
2023-11-25 14:12:39 +0000

    deploy: 8520504983b807215a0b952078b581e430493c54

--- Divisibility.lean
+++ Divisibility.lean
@@ -2,6 +2,8 @@ import Mathlib.Data.Int.Basic --#
 import Mathlib.Algebra.Order.Group.Abs --#
 import Mathlib.Algebra.Order.Ring.CharZero --#
 
+section --#
+
 /-! # 整除関係
 
 ## 定義
@@ -11,14 +13,16 @@ import Mathlib.Algebra.Order.Ring.CharZero --#
 整数 $a, b ∈ ℤ$ が与えられたとします．このとき $a$ が $b$ を割り切るとは，ある $z ∈ ℤ$ が存在して $a z = b$ となることを言います．これを $a ∣ b$ と書きます．
 -/
 
-/-! 「$a$ が $b$ を割り切る」というのは $a$ 目線の言い方ですが，$b$ 目線では「$b$ は $a$ の倍数(multiple)である」と言います．
-また，$a$ で $b$ が割り切れないとき，斜線を入れて $a ∤ b$ と書きます．-/
+/-! $a ∣ b$ であることを次のようにも言いますが，すべて同じ意味です．
+* $b$ は $a$ で割り切れる
+* $b$ は $a$ の倍数(multiple)である
+* $a$ は $b$ の約数(divisor)である
+
+$a ∣ b$ でないとき，斜線を入れて $a ∤ b$ と書きます．-/
 
 /-! ## Lean での定義
 
 Lean では，「割り切れる」ということは `Dvd` という型クラスで表現されます．その際，`∣` という記法も定義されています． -/
-
-section --#
 
 -- inductive Dvd.{u_1} : Type u_1 → Type u_1
 -- number of parameters: 1
@@ -30,17 +34,17 @@ variable (α : Type) [self : Dvd α]
 
 #check ((· ∣ ·) : α → α → Prop)
 
-end --#
+/-! そして整数 `Int` や自然数 `Nat` は `Dvd` のインスタンスです. -/
 
-/-! そして整数 `Int` は `Dvd` のインスタンスです．これは `Int.instDvdInt` という名前で呼ばれています． -/
+-- Int.instDvdInt
+#synth Dvd Int
 
--- def Int.instDvdInt : Dvd ℤ :=
--- { dvd := fun a b => ∃ c, b = a * c }
-#print Int.instDvdInt
-
-#check ((· ∣ ·) : ℤ → ℤ → Prop)
+-- Nat.instDvdNat
+#synth Dvd Nat
 
 /-! ## 基本的な性質
+
+以下で紹介する性質は `ℤ` だけでなく `ℕ` でも成り立ちますが，今回は代表して `ℤ` の話として説明します．
 
 定義からすぐに従う基本的な性質として，まずどんな整数 $a ∈ℤ$ に対しても $1 ∣ a$ であり，かつ $a ∣ a$ です．
 -/
@@ -61,13 +65,43 @@ theorem dvd_zero (a : ℤ) : a ∣ 0 := by
   use 0
   simp
 
-/-! さらに，整数 $a, b, c ∈ ℤ$ について $a b ∣ c$ ならば $a ∣ c$ が成り立ちます． -/
+/-! さらに，整数 $a, b, c ∈ ℤ$ について $a b ∣ c$ ならば $a ∣ c$ かつ $b ∣ c$ も成り立ちます． -/
 
-theorem dvd_of_mul_right_dvd (a b c : ℤ) : a * b ∣ c → a ∣ c := by
+theorem dvd_of_mul_right_dvd (a b c : ℤ) : a * b ∣ c → a ∣ c ∧ b ∣ c := by
   -- `a * b ∣ c` だと仮定します
   intro h
+
   -- 整除関係の定義から， `c = a * b * k` となる `k` が取れます．
   have ⟨k, hk⟩ := h
-  -- したがって `a ∣ c` であることが従います．
-  use b * k
-  simp [hk, mul_assoc]
+  clear h
+
+  constructor
+  case left =>
+    -- したがって `a ∣ c` であることが従います．
+    use b * k
+    simp [hk, mul_assoc]
+
+  case right =>
+    -- 同様に `b ∣ c` であることも従います．
+    use a * k
+    simp [hk, ←mul_assoc]
+    rw [show a * b = b * a from by simp [mul_comm]]
+
+/-! ## 演習問題
+次の `sorry` の部分を埋めてみてください．
+-/
+
+variable (a b c : ℤ)
+
+-- 割り算の定義の確認
+example : a ∣ b ↔ ∃ c : ℤ, b = a * c := by
+  sorry
+
+-- 倍数に何か乗じても倍数
+example (h : a ∣ b) : a ∣ b * c := by
+  sorry
+
+example : ∃ a, 11 ∣ (2 ^ a + 1) := by
+  sorry
+
+end --#
